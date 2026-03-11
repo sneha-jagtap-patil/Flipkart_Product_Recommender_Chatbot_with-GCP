b2c91b125e562fa5fbe0cb8c7a9263d1699c7619
sneha jagtap patil <sapatil2115@gmail.com>
2026-03-11 13:01:56 +0530

    update

---
+++ flipkart/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    ASTRA_DB_API_ENDPOINT = os.getenv("ASTRA_DB_API_ENDPOINT")
+    ASTRA_DB_APPLICATION_TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+    ASTRA_DB_KEYSPACE = os.getenv("ASTRA_DB_KEYSPACE")
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    EMBEDDING_MODEL = "BAAI/bge-base-en-v1.5" # it is reasining and tool calling model ..S
+    RAG_MODEL = "groq:qwen/qwen3-32b"
+
